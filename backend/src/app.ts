2bc088f8732f014d43de69ea777f44ace2901502
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-10-02 18:36:13 +0530

    Make first API request to get all users

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { config } from "dotenv";
 import morgan from "morgan";
+import appRouter from "./routes/index.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.use("/api/v1", appRouter);
 
 config();
 
