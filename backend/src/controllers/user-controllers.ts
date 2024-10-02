6313cf699c3da8f804f80df51f18f0a130a968fb
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-10-02 19:39:20 +0530

    Create sign up validations

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -26,7 +26,7 @@ export const userSignUp = async (
     const hashedPassword = await hash(password, 10);
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
-    return res.status(200).json({ message: "OK", id:user._id.toString() });
+    return res.status(201).json({ message: "OK", id:user._id.toString() });
   } catch (error) {
     console.log(error);
     return res.status(200).json({ message: "Error", cause: error.message });
---
+++ backend/src/utils/validators.ts
@@ -0,0 +1,22 @@
+import { body, ValidationChain, validationResult } from "express-validator";
+import { Request, Response, NextFunction } from "express";
+
+export const validate = (validations: ValidationChain[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    await Promise.all(validations.map((validation) => validation.run(req)));
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
+    return res.status(422).json({ errors: errors.array() });
+  };
+};
+
+export const signUpValidator = [
+  body("name").notEmpty().withMessage("Name is required"),
+  body("email").trim().isEmail().withMessage("Email is required"),
+  body("password")
+    .notEmpty()
+    .isLength({ min: 6 })
+    .withMessage("Password should contain atleast 6 characters"),
+];
